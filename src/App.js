a8ee968f708ceec97b7c7594c76f36a18d3625e8
abm6 <55770187+abm6@users.noreply.github.com>
2022-07-15 12:38:04 +0530

    refactor and reduce complexity

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import { useRef, useState, useEffect } from 'react';
 
 import Header from './components/Header.comp';
-import Body from './components/body.comp';
-import Footer from './components/footer.comp';
+import Body from './components/Body.comp';
+import Footer from './components/Footer.comp';
+
+// find the index whose value is closest to the current scroll position
+const closestIndex = (num, arr) => {
+	let curr = arr[0],
+		diff = Math.abs(num - curr);
+	let index = 0;
+	for (let val = 0; val < arr.length; val++) {
+		let newdiff = Math.abs(num - arr[val]);
+		if (newdiff < diff) {
+			diff = newdiff;
+			curr = arr[val];
+			index = val;
+		}
+	}
+	return index;
+};
 
 function App() {
 	const refs = {
@@ -12,60 +28,20 @@ function App() {
 	};
 
 	const NavBar = () => {
-		const [mySection, setMySection] = useState('header');
+		const [mySection, setMySection] = useState(0);
+
+		const navItems = ['header', 'body', 'footer'];
 
 		const handleScroll = () => {
-			// refactors
-
 			const offsetHeights = [];
+
 			Object.values(refs).forEach((val) => {
 				offsetHeights.push(val.current.offsetTop);
 			});
 
-			console.log(offsetHeights);
-
-			const headerOffsetHeight = refs.header.current.offsetHeight;
-			const bodyOffsetHeight = refs.body.current.offsetHeight;
-			const footerOffsetHeight = refs.footer.current.offsetHeight;
-
-			const maxScrollableHeight =
-				document.documentElement.scrollHeight -
-				document.documentElement.clientHeight;
-
-			const totalOffsetHeight = offsetHeights.reduce((a, b) => a + b, 0);
-
 			const position = window.pageYOffset;
-
-			const scrollPercent = Math.round(position) / maxScrollableHeight;
-
-			const calculatedOffsetHeight = scrollPercent * totalOffsetHeight;
-
-			const headTopPosition = 0;
-			const bodyTopPosition = headTopPosition + headerOffsetHeight;
-			const footerTopPosition = bodyTopPosition + bodyOffsetHeight;
-
-			console.log('calculatedOffsetHeight', calculatedOffsetHeight);
-			console.log(
-				Math.round(position),
-				maxScrollableHeight,
-				scrollPercent,
-				totalOffsetHeight
-			);
-			if (calculatedOffsetHeight < bodyTopPosition) {
-				setMySection('header');
-			}
-			if (
-				calculatedOffsetHeight > bodyTopPosition &&
-				calculatedOffsetHeight < bodyTopPosition + bodyOffsetHeight
-			) {
-				setMySection('body');
-			}
-			if (
-				calculatedOffsetHeight > bodyTopPosition + bodyOffsetHeight &&
-				calculatedOffsetHeight <= totalOffsetHeight
-			) {
-				setMySection('footer');
-			}
+			const activeSectionIndex = closestIndex(position, offsetHeights);
+			setMySection(activeSectionIndex);
 		};
 
 		useEffect(() => {
@@ -79,70 +55,36 @@ function App() {
 		return (
 			<nav>
 				<ul>
-					<li
-						className={mySection === 'header' ? 'active' : ''}
-						onClick={() => scrollToSection(refs.header)}>
-						Header
-					</li>
-					<li
-						className={mySection === 'body' ? 'active' : ''}
-						onClick={() => scrollToSection(refs.body)}>
-						Body
-					</li>
-					<li
-						className={mySection === 'footer' ? 'active' : ''}
-						onClick={() => scrollToSection(refs.footer)}>
-						Footer
-					</li>
+					{navItems.map((val, index) => {
+						return (
+							<li
+								key={index}
+								className={mySection === index ? 'active' : ''}
+								onClick={() => scrollToSection(val)}>
+								{val}
+							</li>
+						);
+					})}
 				</ul>
 			</nav>
-			// <div className="section-status">
-			// 	<h3>Focused Section: </h3>
-			// 	<p>{mySection}</p>
-			// </div>
 		);
 	};
 
-	const getSection = () => {
-		console.log(refs.header);
-	};
-
-	const handleScrollEvent = (setMySection) => {
-		const scrollTop = window.pageYOffset;
-		const headerHeight = refs.header.current.offsetHeight;
-		const bodyHeight = refs.body.current.offsetHeight;
-		const footerHeight = refs.footer.current.offsetHeight;
-		const totalHeight = headerHeight + bodyHeight + footerHeight;
-
-		if (scrollTop < headerHeight) {
-			setMySection('header');
-		} else if (
-			scrollTop >= headerHeight &&
-			scrollTop < totalHeight - footerHeight
-		) {
-			setMySection('body');
-		} else {
-			setMySection('footer');
-		}
-	};
-
-	const scrollToSection = (elementRef) => {
+	const scrollToSection = (sectionName) => {
 		window.scrollTo({
-			top: elementRef.current.offsetTop,
+			top: refs[sectionName].current.offsetTop,
 			behavior: 'smooth',
 		});
 	};
 
-	const handleGoToSection = (sectionName) => scrollToSection(refs[sectionName]);
 
 	return (
 		<div className="container">
 			<NavBar />
-
 			<section>
-				<Header ref={refs.header} goToSection={handleGoToSection} />
-				<Body ref={refs.body} goToSection={handleGoToSection} />
-				<Footer ref={refs.footer} goToSection={handleGoToSection} />
+				<Header ref={refs.header} goToSection={scrollToSection} />
+				<Body ref={refs.body} goToSection={scrollToSection} />
+				<Footer ref={refs.footer} goToSection={scrollToSection} />
 			</section>
 		</div>
 	);
